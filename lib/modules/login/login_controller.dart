313b9a3ee0bb09821384995035c56aff4d023543
Ricardo Augusto <ricardoaugusto.eb@gmail.com>
2022-05-03 18:23:33 -0300

    add logout page

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
 import 'package:payflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: [
@@ -31,6 +32,16 @@ class LoginController {
 
   // Implementar - Função de Logout
   Future<void> googleSignOut(BuildContext context) async {
-    await _googleSignIn.disconnect();
+    try {
+      final response = await _googleSignIn.disconnect();
+      final user =
+          UserModel(name: response!.displayName!, photoURL: response.photoUrl);
+      authController.setUser(context, user);
+      await Future.delayed(Duration(seconds: 3));
+      print(response);
+    } catch (error) {
+      authController.setUser(context, null);
+      print(error);
+    }
   }
 }
--- lib/modules/sign_out/sign_out.dart
+++ lib/modules/sign_out/sign_out.dart
@@ -1,1 +1,52 @@
+import 'dart:async';
 
+import 'package:animated_card/animated_card.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class SignOut extends StatefulWidget {
+  const SignOut({Key? key}) : super(key: key);
+
+  @override
+  _SignOutState createState() => _SignOutState();
+}
+
+class _SignOutState extends State<SignOut> {
+  final controller = LoginController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: AnimatedCard(
+          direction: AnimatedCardDirection.top,
+          child: AlertDialog(
+            title: Text('Sair do PayFlow?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Cancelar',
+                  style: TextStyles.buttonBoldHeading,
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await controller.googleSignOut(context);
+                },
+                child: Text(
+                  'Sair',
+                  style: TextStyles.buttonBoldPrimary,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
